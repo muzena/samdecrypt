84d36a721a5c8d11adbf2a1dd964d03f95d614bc
goran <trebelnik2@gmail.com>
2014-07-01 00:14:24 +0200

    Add export/import tv channel function

---
+++ import-TV-configuration.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+#set -e
+set -x  debug
+
+
+####################################################################################
+
+#echo "Checking and creating if it need user data file."
+#FILE=~/.decrypt_userdata
+#if [ -f $FILE ]; then
+#   echo "File '$FILE' exists"
+#else
+#   echo 'usb="sda1"
+#tvip="ENTER_YOUR_TV_IP"' > ~/.decrypt_userdata
+#fi
+echo "Selecting TV ip address and recording devices."
+usb=""
+tvip=""
+path=""
+widgetpath=""
+#target="/usr/share/samdecrypt/.decrypt_userdata"
+#M_OPTS="$PERM,usb=${USER},tvip=${PASSWD},codepage=cp1250,iocharset=utf8"
+[ -e /usr/share/samdecrypt/.decrypt_userdata ] && source /usr/share/samdecrypt/.decrypt_userdata
+[ -z $usb ] && exit 1
+[ -z $tvip ] && exit 1
+[ -z $path ] && exit 1
+[ -z $widgetpath ] && exit 1
+
+####################################################################################
+# notify-send --app-name="Samdecrypt" --expire-time="3000" --icon="/usr/share/pixmaps/samdecrypt.png" "Uploading widget to Samsung TV..."
+
+function list_widget()
+{
+nc  -t -i 1 $tvip 23 <<EOF
+cd ..
+cd /mtd_rwcommon/widgets/user/
+ls -d * > list.txt
+exit
+EOF
+}
+function remove_widgetlist()
+{
+nc  -t -i 1 $tvip 23 <<EOF
+cd ..
+cd /mtd_rwcommon/widgets/user/
+rm list.txt
+rm makezip
+rm $choicewidget.tar
+exit
+EOF
+}
+################################################################################################
+# make makezip files for putting into tv
+function put_tvconfig()
+{
+ftp -in $tvip <<EOF
+lcd $path/tv-config
+cd /mtd_rwarea
+binary
+put FineTune_Digital
+put map-AirA
+put map-AirD
+put map-CableD
+put map-ChKey
+put PackageDataBase.dat
+put RadioMap
+put SatDataBase.dat
+put ServiceProviders
+put TransponderDataBase.dat
+put TSIDList
+put UserTransponderDataBase.dat
+quit
+EOF
+}
+function unpack_scmconfig()
+{
+cd $path
+cp tv-config.scm -t $path/tv-config
+cd tv-config
+unzip tv-config.scm
+}
+
+mkdir $path/tv-config
+
+# make zip archive for config files
+unpack_scmconfig
+# get config files from TV
+put_tvconfig
+# Remove tmp files
+rm -r $path/tv-config
+wait
+notify-send --app-name="Samdecrypt" --expire-time="3000" --icon="/usr/share/pixmaps/samdecrypt.png" "Samsung TV configuration is imported to TV"
+
+videofolder="xdg-open $path"
+conf3="/usr/share/samdecrypt/reboot-tv"
+
+yad \
+  --title="Samdecrypt" \
+  --window-icon="/usr/share/pixmaps/samdecrypt-24.png" \
+  --width=260 \
+  --height=90 \
+  --text="Samsung TV configuration is imported to TV" \
+  --text-align="center" \
+  --button="Open configuration folder:$videofolder" \
+  --button="Reboot TV:$conf3" \
+  --button="Close:1" \
+
+
+
+sleep 3
+
+
+#echo "Exiting Samdecrypt for "
+#count_time
+#alias alert_helper='history|tail -n1|sed -e "s/^\s*[0-9]\+\s*//" -e "s/;\s*alert$//"'
+#alias alert='notify-send -i /usr/share/icons/gnome/32x32/apps/gnome-terminal.png "[$?] $(alert_helper)"'
+#sleep 20; alert
+
